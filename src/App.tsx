d5de5f5d772e2737e778c7e78563763655523578
Assaf <1214assaf@gmail.com>
2024-06-23 13:36:36 +0300

    refactor: Update Todos component to use Todo model
    The Todos component has been updated to use the Todo model instead of a string array for the items prop. This change allows for better type safety and organization of the code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 function App() {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript'),
+
+  ]
+
   return (
     <div className="App">
-     <Todos items={['tada']}/>
+     <Todos items={todos}/>
     </div>
   );
 }
