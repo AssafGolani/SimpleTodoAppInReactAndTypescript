85c14ff256556ed8aa1fb3199347588571c43140
Assaf <1214assaf@gmail.com>
2024-06-23 17:30:41 +0300

    refactor: Update App component to use TodosContextProvider for managing todos
    This commit refactors the App component to use the TodosContextProvider for managing todos. It removes the useState hook and replaces it with the TodosContextProvider component, which provides the todos state and functions for adding and removing todos to its child components. This change improves code organization and reusability by separating the todos logic into a separate context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
-import Todo from './models/todo';
+import TodosContextProvider from './store/todos-context';
 function App() {
 
-  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
 
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== todoId);
-    });
-  }
+  return ( 
+  <TodosContextProvider>
+    <NewTodo  />
+    <Todos />
+  </TodosContextProvider>
 
-  return ( <>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
-  </>
   );
 }
 
